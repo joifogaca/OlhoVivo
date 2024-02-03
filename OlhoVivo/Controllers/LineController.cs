6400064ab5751172960109dedf9571329c6e34a2
Joice Fogaça <joi.fogaca@gmail.com>
2024-02-02 22:09:12 -0300

    Adicionado ResultViewModel

--- OlhoVivo/Controllers/LineController.cs
+++ OlhoVivo/Controllers/LineController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using OlhoVivo.Data;
 using OlhoVivo.Models;
+using OlhoVivo.ModelViews;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,47 +20,104 @@ namespace OlhoVivo.Controllers
         [HttpGet("v1/lines")]
         public async Task<IActionResult> GetAsync()
         {
-             var lines = await _dataContext.Lines.ToListAsync();
-            return Ok(lines);
+            try
+            {
+                var lines = await _dataContext.Lines.ToListAsync();
+                return Ok(new ResultViewModel<List<Line>>(lines));
+            }
+            catch 
+            {
+
+                return StatusCode(500, new ResultViewModel<List<Line>>("ERROR001 - Falha interna no servidor"));
+            }
+             
         }
 
         // GET api/<LineController>/5
         [HttpGet("v1/lines/{id}")]
         public async Task<IActionResult> GetByIdAsync(int id)
         {
-            var line = await _dataContext.Lines.SingleOrDefaultAsync(x => x.Id == id);
-            return Ok(line);
+            try
+            {
+                var line = await _dataContext.Lines.SingleOrDefaultAsync(x => x.Id == id);
+
+                if(line == null) 
+                    return NotFound(new ResultViewModel<Line>("ERROR002 - Line not found."));
+
+                return Ok(new ResultViewModel<Line>(line));
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, new ResultViewModel<Line>("ERROR003 - Server Error"));
+            }
+            
         }
 
         //POST api/<LineController>
         [HttpPost("v1/lines/")]
         public async Task<IActionResult> PostAsync([FromBody] string name)
         {
-            var line = new Line(name);
-            _dataContext.Lines.Add(line);
-            await _dataContext.SaveChangesAsync();
+            // TODO Fazer verificação do modelo veio correto 
+            try
+            {
+                var line = new Line(name);
+                _dataContext.Lines.Add(line);
+                await _dataContext.SaveChangesAsync();
 
-            return Ok(line);
+                return Ok(new ResultViewModel<Line>(line));
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, new ResultViewModel<Line>("ERROR004 - Server Error"));
+            }
+            
         }
 
         // PUT api/<LineController>/5
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, [FromBody] string name)
         {
-            var line = _dataContext.Lines.SingleOrDefault(x => x.Id == id);
-            line.SetName(name);
-            await _dataContext.SaveChangesAsync();
-            return Ok(line);
+            try
+            {
+                var line = _dataContext.Lines.SingleOrDefault(x => x.Id == id);
+
+                if(line == null) 
+                    return NotFound(new ResultViewModel<Line>("ERROR005 - Line not found."));
+
+                line.SetName(name);
+                await _dataContext.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<Line>(line));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "ERROR006 - Server Error");
+            }
         }
 
         // DELETE api/<LineController>/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            var line = _dataContext.Lines.SingleOrDefault(x => x.Id == id);
-            _dataContext.Lines.Remove(line);
-            await _dataContext.SaveChangesAsync();
-            return Ok();
+            try
+            {
+                var line = _dataContext.Lines.SingleOrDefault(x => x.Id == id);
+                
+                if (line == null)
+                    return NotFound(new ResultViewModel<Line>("ERROR007 - Line not found."));
+
+                _dataContext.Lines.Remove(line);
+                await _dataContext.SaveChangesAsync();
+                return Ok(new ResultViewModel<Line>(line));
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, "ERROR006 - Server Error");
+            }
+           
         }
     }
 }
---
+++ OlhoVivo/ModelViews/ResultViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace OlhoVivo.ModelViews
+{
+    public class ResultViewModel<T>
+    {
+        public ResultViewModel(T data)
+        {
+            Data = data;
+        }
+
+        public ResultViewModel(string error)
+        {
+            Errors.Add(error);
+        }
+        public ResultViewModel(T data, List<string> errors) {
+        Data = data;
+        }
+
+        public T Data { get; private set; }
+
+        public List<string> Errors { get; private set; } = new();
+    }
+}
